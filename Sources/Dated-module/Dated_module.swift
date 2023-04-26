589b80691c0c289baaa201c3c537bf5af5a04ff1
Jeremy Bannister <jeremy.a.bannister@gmail.com>
2023-04-26 14:10:33 +0200

    Initial Commit

---
+++ Sources/Dated-module/Dated_module.swift
@@ -0,0 +1,6 @@
+public struct Dated_module {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ Tests/Dated-moduleTests/Dated_moduleTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import Dated_module
+
+final class Dated_moduleTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(Dated_module().text, "Hello, World!")
+    }
+}
